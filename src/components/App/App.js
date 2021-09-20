d1ae5a5c882093c80ed71e6a0649221b88c722e6
Zach Johnson <zachjjohns@gmail.com>
2021-09-20 11:30:05 -0500

    Create folders for components, add header, change default colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Spacestagram</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Spacestagram</h1>
+    </header>
+  )
+}
