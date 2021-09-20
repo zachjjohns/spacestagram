6a85d63fe34701edc2db815fc6e2c64782a77929
Zach Johnson <zachjjohns@gmail.com>
2021-09-20 11:55:06 -0500

    Create CardContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Header from '../Header/Header';
+import "./App.css";
+import Header from "../Header/Header";
+import CardContainer from "../CardContainer/CardContainer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardContainer />
     </div>
   );
 }
---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,11 @@
+import "./CardContainer.css";
+
+import React from 'react'
+
+export default function CardContainer() {
+  return (
+    <main>
+      <p>Cards go here!</p>
+    </main>
+  )
+}
