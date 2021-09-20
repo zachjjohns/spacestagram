4f0af7731fda790e7a8b8e2de81d7984dbfe3bb2
Zach Johnson <zachjjohns@gmail.com>
2021-09-20 15:37:44 -0500

    Add JSX for title, date, description

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,8 +4,10 @@ import React from "react";
 export default function Card({ title, mediaType, url, date, explanation }) {
   return (
     <div className="card-container">
-      <span></span>
-      <img alt={title} src={url} />
+      <img alt={title} src={url} className="card-media"/>
+      <p className="card-title">{title}</p>
+      <p className="card-date">{date}</p>
+      <p className="card-desc">{explanation}</p>
     </div>
   )
 }
