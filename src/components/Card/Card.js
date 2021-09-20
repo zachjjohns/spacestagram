c824a4124ec514bc6c20b8867ef9ff88ab7b0642
Zach Johnson <zachjjohns@gmail.com>
2021-09-20 17:31:09 -0500

    Fix description boxes/scrolling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,8 +6,12 @@ export default function Card({ title, mediaType, url, date, explanation }) {
     <div className="card-container">
       <img alt={title} src={url} className="card-media"/>
       <p className="card-title">{title}</p>
-      <p className="card-date">{date}</p>
+      <p className="card-date">Date Taken: {date}</p>
       <p className="card-desc">{explanation}</p>
+      <div className="btn-div">
+        <button className="like-btn">Like</button>
+        <button className="share-btn">Share</button>
+      </div>
     </div>
   )
 }
